14471e64fc4fa20b0fd520271b6695691520b6c5
Harry <harry_emmett@outlook.com>
2024-08-06 01:13:35 +0100

    feat: more practise

--- src/recursiveMethodsPractise.js
+++ src/recursiveMethodsPractise.js
@@ -1,143 +1,235 @@
-const powerLoop = (x, n) => {
-  let result = 1;
+// const powerLoop = (x, n) => {
+//   let result = 1;
 
-  for (let i = 0; i < n; i++) {
-    result *= x;
-  }
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
 
-  return result;
-};
+//   return result;
+// };
 
-const powerRecursive = (x, n) => {
-  if (n === 1) {
-    return x;
-  } else {
-    return x * powerRecursive(x, n - 1);
-  }
-};
+// const powerRecursive = (x, n) => {
+//   if (n === 1) {
+//     return x;
+//   } else {
+//     return x * powerRecursive(x, n - 1);
+//   }
+// };
 
-console.log(powerLoop(4, 2));
-console.log(powerRecursive(4, 2));
+// console.log(powerLoop(4, 2));
+// console.log(powerRecursive(4, 2));
 
-// Recursive traversal
-let company = {
-  sales: [
-    { name: "John", salary: 1000 },
-    { name: "Alice", salary: 1600 },
-  ],
-  development: {
-    sites: [
-      { name: "Peter", salary: 2000 },
-      { name: "Alex", salary: 1800 },
-    ],
-    new: [{ name: "Bob", salary: 300 }],
-    internals: [{ name: "Jack", salary: 1300 }],
-  },
-};
+// // Recursive traversal
+// let company = {
+//   sales: [
+//     { name: "John", salary: 1000 },
+//     { name: "Alice", salary: 1600 },
+//   ],
+//   development: {
+//     sites: [
+//       { name: "Peter", salary: 2000 },
+//       { name: "Alex", salary: 1800 },
+//     ],
+//     new: [{ name: "Bob", salary: 300 }],
+//     internals: [{ name: "Jack", salary: 1300 }],
+//   },
+// };
 
-const sumSalaries = (companies) => {
-  if (Array.isArray(companies)) {
-    return companies.reduce((prev, curr) => prev + curr.salary, 0);
-  } else {
-    let sum = 0;
-    for (let department of Object.values(companies)) {
-      sum += sumSalaries(department);
-    }
-    return sum;
-  }
-};
+// const sumSalaries = (companies) => {
+//   if (Array.isArray(companies)) {
+//     return companies.reduce((prev, curr) => prev + curr.salary, 0);
+//   } else {
+//     let sum = 0;
+//     for (let department of Object.values(companies)) {
+//       sum += sumSalaries(department);
+//     }
+//     return sum;
+//   }
+// };
 
-console.log(sumSalaries(company));
+// console.log(sumSalaries(company));
 
-const sumTo = (number) => {
-  if (number === 1) return number;
+// const sumTo = (number) => {
+//   if (number === 1) return number;
 
-  return number + sumTo(number - 1);
-};
+//   return number + sumTo(number - 1);
+// };
 
-console.log(sumTo(1), sumTo(2), sumTo(3), sumTo(4), sumTo(100));
+// console.log(sumTo(1), sumTo(2), sumTo(3), sumTo(4), sumTo(100));
 
-const factorial = (number) => {
-  if (number === 1) return number;
+// const factorial = (number) => {
+//   if (number === 1) return number;
 
-  return number * factorial(number - 1);
-};
+//   return number * factorial(number - 1);
+// };
 
-console.log(
-  factorial(1),
-  factorial(2),
-  factorial(3),
-  factorial(4),
-  factorial(5)
-);
+// console.log(
+//   factorial(1),
+//   factorial(2),
+//   factorial(3),
+//   factorial(4),
+//   factorial(5)
+// );
 
-function fibonacci(n) {
-  let a = 1;
-  let b = 1;
-  for (let i = 3; i <= n; i++) {
-    let c = a + b;
-    a = b;
-    b = c;
-  }
-  return b;
-}
+// function fibonacci(n) {
+//   let a = 1;
+//   let b = 1;
+//   for (let i = 3; i <= n; i++) {
+//     let c = a + b;
+//     a = b;
+//     b = c;
+//   }
+//   return b;
+// }
 
-console.log(fibonacci(3), fibonacci(77));
+// console.log(fibonacci(3), fibonacci(77));
 
-let list = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: {
-        value: 4,
-        next: null,
+// let list = {
+//   value: 1,
+//   next: {
+//     value: 2,
+//     next: {
+//       value: 3,
+//       next: {
+//         value: 4,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+// const printListRecursion = (list) => {
+//   console.log(list.value);
+//   if (list.next === null) return;
+
+//   return printListRecursion(list.next);
+// };
+
+// printListRecursion(list);
+
+// const printListLoop = (listItem) => {
+//   let temp = listItem;
+
+//   while (temp) {
+//     console.log(temp.value);
+//     temp = temp.next;
+//   }
+// };
+
+// printListLoop(list);
+
+// const reverseLinkedListRecursion = (linkedList) => {
+//   if (linkedList.next) reverseLinkedListRecursion(linkedList.next);
+
+//   console.log(linkedList.value);
+// };
+
+// reverseLinkedListRecursion(list);
+
+// const reverseLinkedListLeep = (linkedList) => {
+//   const items = [];
+//   let temp = linkedList;
+
+//   while (temp) {
+//     items.push(temp.value);
+//     temp = temp.next;
+//   }
+
+//   for (let i = items.length - 1; i >= 0; i--) {
+//     console.log(items[i]);
+//   }
+// };
+
+// reverseLinkedListLeep(list);
+
+// const sumRange = (number) => {
+//   if (number === 1) return 1;
+
+//   console.log(number);
+//   return number + sumRange(number - 1);
+// };
+
+// sumRange(3);
+
+// const allAreLessThanSeven = (array) => {
+//   const newArray = [...array];
+
+//   if (newArray.length === 0) return;
+//   if (newArray[newArray.length - 1] > 7) return false;
+
+//   newArray.pop();
+//   allAreLessThanSeven(newArray);
+//   return true;
+// };
+
+// console.log(allAreLessThanSeven([1, 5, 1, 3, 4, 3, 2, 7]));
+
+// const allAreLessThanSevenCallback = (array, callback) => {
+//   const newArray = [...array];
+
+//   all elements poped so everyone is < 7 return true
+//   if (newArray.length === 0) return true;
+
+//   if (callback(newArray[newArray.length - 1])) {
+//     newArray.pop();
+//     return allAreLessThanSevenCallback(newArray, callback);
+//   } else {
+//     element > 7 found exit case
+//     newArray.length = 0;
+//     return false;
+//   }
+// };
+
+// const value = allAreLessThanSevenCallback(
+//   [1, 5, 1, 3, 4, 8, 2, 6, 6],
+//   (val) => {
+//     return val < 7;
+//   }
+// );
+
+// console.log(value);
+
+// const productOfArray = (array) => {
+//   const currentArray = [...array];
+
+//   if (currentArray.length === 1) return currentArray.pop();
+
+//   const value = currentArray.pop();
+//   return value * productOfArray(currentArray);
+// };
+
+// console.log(productOfArray([1, 2, 3]));
+// console.log(productOfArray([1, 2, 3, 10]));
+
+const nestedObject = {
+  data: {
+    info: {
+      stuff: {
+        thing: {
+          moreStuff: {
+            magicNumber: 44,
+            something: "foo2",
+            bob: "bob",
+          },
+        },
       },
     },
   },
 };
 
-const printListRecursion = (list) => {
-  console.log(list.value);
-  if (list.next === null) return;
+const contains = (nested, value) => {
+  if (typeof nested !== "object" || nested === null) return nested === value;
 
-  return printListRecursion(list.next);
-};
+  for (let k of Object.keys(nested)) {
+    const hasValue = contains(nested[k], value);
 
-printListRecursion(list);
-
-const printListLoop = (listItem) => {
-  let temp = listItem;
-
-  while (temp) {
-    console.log(temp.value);
-    temp = temp.next;
-  }
-};
-
-printListLoop(list);
-
-const reverseLinkedListRecursion = (linkedList) => {
-  if (linkedList.next) reverseLinkedListRecursion(linkedList.next);
-
-  console.log(linkedList.value);
-};
-
-reverseLinkedListRecursion(list);
-
-const reverseLinkedListLeep = (linkedList) => {
-  const items = [];
-  let temp = linkedList;
-
-  while (temp) {
-    items.push(temp.value);
-    temp = temp.next;
+    if (hasValue) return true;
   }
 
-  for (let i = items.length - 1; i >= 0; i--) {
-    console.log(items[i]);
-  }
+  return false;
 };
 
-reverseLinkedListLeep(list);
+console.log(contains(nestedObject, 44));
+console.log(contains(nestedObject, "foo"));
+console.log(contains(nestedObject, "foo2"));
+console.log(contains(nestedObject, "bob"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/recursiveMethods.js",
+  entry: "./src/recursiveMethodsPractise.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
