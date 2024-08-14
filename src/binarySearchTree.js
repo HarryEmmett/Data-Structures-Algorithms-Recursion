7f572445bf3b8b759c77c31379e22a889ccebee3
Harry <harry_emmett@outlook.com>
2024-08-14 23:47:38 +0100

    refactoring

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,7 +1,7 @@
 class Node {
-  constructor(d, l = undefined, r = undefined) {
-    this.left = l;
-    this.right = r;
+  constructor(d) {
+    this.left = null;
+    this.right = null;
     this.data = d;
   }
 }
@@ -21,7 +21,7 @@ class Tree {
   }
 
   buildTree(array) {
-    if (array.length === 0) return;
+    if (array.length === 0) return null;
     const midPoint = Math.floor((array.length - 1) / 2);
     const node = new Node(array[midPoint]);
     node.left = this.buildTree(array.slice(0, midPoint));
@@ -41,18 +41,16 @@ class Tree {
   }
 
   // need a separate recursive function to insert as if the base case gets to undefined the default parameter will be used instead of undefined
-  i(value, array) {
-    if (array === undefined) {
+  i(value, node) {
+    if (node === null) {
       return new Node(value);
     }
-
-    if (value > array.data) {
-      array.right = this.i(value, array.right);
-    } else if (value < array.data) {
-      array.left = this.i(value, array.left);
+    if (value > node.data) {
+      node.right = this.i(value, node.right);
+    } else if (value < node.data) {
+      node.left = this.i(value, node.left);
     }
-
-    return array;
+    return node;
   }
 
   delete(value) {
@@ -60,47 +58,76 @@ class Tree {
     this.array = this.d(value, this.array);
   }
 
-  d(value, array) {
-    if (array === undefined) {
-      return array;
+  d(value, node) {
+    if (node === null) {
+      return node;
     }
 
-    if (value > array.data) {
-      array.right = this.d(value, array.right);
-    } else if (value < array.data) {
-      array.left = this.d(value, array.left);
-    }
-
-    if (array.data === value) {
-      if (array.left || array.right) {
+    if (value > node.data) {
+      node.right = this.d(value, node.right);
+    } else if (value < node.data) {
+      node.left = this.d(value, node.left);
+    } else {
+      // if this block gets hit it means that an equal value has been hit.
+      // a high value that is not in the tree will just hit the recursion on 69 until returning undefined
+      if (node.left || node.right) {
         // One child node
-        if (array.left === undefined) return array.right;
-        if (array.right === undefined) return array.left;
+        if (node.left === null) return node.right;
+        if (node.right === null) return node.left;
 
         // more than one child
-        let min;
-        let temp = array;
-        while (temp.right && temp.left) {
-          min = temp.right.data;
+
+        // in order sucessor is the smallest node in the right node of the one you want to delete
+        let temp = node.right;
+        while (temp.left !== null) {
           temp = temp.left;
         }
 
-        array.data = min;
-        array.right = this.d(min, array.right);
-        return array;
+        node.data = temp.data;
+        node.right = this.d(temp.data, node.right);
+        return node;
       } else {
-        array = undefined;
+        // no children so return undefined
+        return undefined;
       }
     }
 
-    return array;
+    return node;
+  }
+
+  find(value, node = this.array) {
+    if (node === null) {
+      return undefined;
+    }
+
+    if (node.data === value) return node;
+
+    if (value > node.data) {
+      return this.find(value, node.right);
+    } else if (value < node.data) {
+      return this.find(value, node.left);
+    }
+  }
+  levelOrder(callback) {
+    if (typeof callback !== "function")
+      return "Please provide a callback function";
+
+    const bst = this.array;
+    const queue = [];
+
+    queue[0] = bst.data;
+
+    while (queue.length > 0) {
+      const value = queue.pop();
+      const node = this.find(value);
+    }
   }
 
   prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === undefined) {
+    if (node === null) {
       return;
     }
-    if (node.right !== undefined) {
+    if (node.right !== null) {
       this.prettyPrint(
         node.right,
         `${prefix}${isLeft ? "│   " : "    "}`,
@@ -108,7 +135,7 @@ class Tree {
       );
     }
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== undefined) {
+    if (node.left !== null) {
       this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
   };
@@ -117,16 +144,10 @@ class Tree {
 // const a = new Tree();
 const a = new Tree([2]);
 const tree = a.root();
-a.insert(100);
-a.insert(90);
-a.insert(95);
-a.insert(96);
-a.insert(98);
-a.insert(110);
-a.insert(93);
-a.insert(91);
-a.insert(94);
+a.insert(10);
+a.insert(5);
+a.insert(1);
+a.insert(20);
+a.insert(15);
 a.prettyPrint(tree);
-// a.delete(95);
-a.delete(100);
-a.prettyPrint(tree);
+a.levelOrder((callback) => console.log("my callback", callback));
